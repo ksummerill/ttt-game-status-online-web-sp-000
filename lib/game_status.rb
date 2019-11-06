dee0b4b62546c9684a694c322ec90b78ec488d45
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-06 16:53:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,16 +29,9 @@ def won?(board)
   winner
 end
 
-# # check to see if the board is full
-# def full?(board)
-#   !board.all? do { |token| board[]}
-#
-#   end
-#   { "X" || "O" }
-# end
-
+# check to see if the board is full
 def full?(board)
-  !board.all? { |full| full == "X" || "O"}
+  !board.all? { |full| full == "X" || full == "O"}
 end
 
 # returns true if the board has not been won but is full,
